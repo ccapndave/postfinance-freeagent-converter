e18fe5cb01c91454315491730d114f36e133e2f8
Dave Keen <dev@ruffness.com>
2016-05-02 15:45:36 +0200

    Updated for the CSVs exported from the e-finance web interface (as the Java app no longer exists)

---
+++ convert.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+const result =
+    fs.readFileSync(`${__dirname}/new.csv`, { encoding: 'utf-8' })
+        // Split the file into an array by line endings
+        .split("\r\n")
+        
+        // We only care about lines that start with a digit (they are the transactions)
+        .filter(line => line.match(/^[0-9].*$/))
+        
+        // Transform the line into the format that Freeagent wants
+        .map(line => {
+            const row = line.split(";");
+            
+            const credit = parseFloat(row[2].replace("'", "")) || 0;
+            const debit = parseFloat(row[3].replace("'", "")) || 0;
+            const details = row[1];
+            const date = row[0].replace(/\./g, "/");
+            
+            // Since credit and debit contain the sign (+ or -) we can just add them together as parseInt will already have dealt with it
+            return [ date, credit + debit, details ];
+        })
+        
+        // And turn it back into rows
+        .map(row => row.join(","))
+        
+        // And finally a string;
+        .join("\n");
+
+console.log(result);
